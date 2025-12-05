de8bd0d9380ec1597610b86126940ca7fe23343f
Eduardo Bonto <eduardobonto@gmail.com>
2025-12-05 15:17:12 +0800

    Initial Commit

---
+++ McdDelivery/McdDeliveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  McdDeliveryApp.swift
+//  McdDelivery
+//
+//  Created by Mac on 2025-12-05.
+//
+
+import SwiftUI
+
+@main
+struct McdDeliveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
