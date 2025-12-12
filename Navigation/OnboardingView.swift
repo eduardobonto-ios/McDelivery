8afc45657523ec9d530421e894840e2080d77c80
Eduardo Bonto <eduardobonto@gmail.com>
2025-12-12 15:10:40 +0800

    first commit

---
+++ Navigation/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  McdDelivery
+//
+//  Created by Mac on 2025-12-11.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ Navigation/SideMenu.swift
@@ -0,0 +1,8 @@
+//
+//  SideMenu.swift
+//  McdDelivery
+//
+//  Created by Mac on 2025-12-05.
+//
+
+import Foundation
